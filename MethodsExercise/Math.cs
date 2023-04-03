2ff2366f2cf27038205a0f42fb04a15840434831
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-04-03 01:45:02 -0500

    completed

---
+++ MethodsExercise/Math.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    public class MathMethods
+    {
+        public static int Sum(int[] nums)
+        {
+            int value = 0;
+            foreach (int i in nums)
+            {
+                value += i;
+            }
+            return value;
+        }
+
+        public static int Subtract(int[] nums)
+        {
+            int value = 0;
+            foreach (int i in nums)
+            {
+                value -= i;
+            }
+            return value;
+        }
+
+        public static int Multiply(int[] nums)
+        {
+            int value = 1;
+            foreach (int i in nums)
+            {
+                value *= i;
+            }
+            return value;
+        }
+        public static decimal Divide(int[] nums)
+        {
+            int value = 0;
+            foreach (int i in nums)
+            {
+                value /= i;
+            }
+            return value;
+        }
+
+        
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            /* //exercise 1
+             Console.WriteLine("enter an adjective");
+             string adjective1 = Console.ReadLine();
+             Console.WriteLine("enter an adjective");
+             string adjective2 = Console.ReadLine();
+             Console.WriteLine("enter an adjective");
+             string adjective3 = Console.ReadLine();
+             Console.WriteLine("enter an adjective");
+             string adjective4 = Console.ReadLine();
+             Console.WriteLine("enter an adjective");
+             string adjective5 = Console.ReadLine();
+             Console.WriteLine("enter an adjective");
+             string adjective6 = Console.ReadLine();
+             Console.WriteLine("enter an adjective");
+             string adjective7 = Console.ReadLine();
+             Console.WriteLine("enter an adjective");
+             string adjective8 = Console.ReadLine();
+             Console.WriteLine("enter a noun");
+             string noun1 = Console.ReadLine();
+             Console.WriteLine("enter a noun");
+             string noun2 = Console.ReadLine();
+             Console.WriteLine("enter a noun");
+             string noun3 = Console.ReadLine();
+             Console.WriteLine("enter a verb");
+             string verb1 = Console.ReadLine();
+             Console.WriteLine($"Once upon a {adjective1} time, in a {adjective2} land far, far away, a {noun1} went on a wild adventure. They found a magical {adjective3} {noun2} and suddenly ended up in a {adjective4} world filled with {noun3}.\r\n\r\nAs they {verb1} through the {adjective5} landscape, they encountered {adjective6} creatures and a {adjective7} danger. Will they make it back home safely? Who knows, but it's sure to be a {adjective8} time!");
+             */
+            
+             //exercise 2
+             var intArray = new int[] { 2, 3, 4 };
+             var x = MathMethods.Sum(intArray);
+             Console.WriteLine(x);
+             var intArray2 = new int[] { 3, 51, 15 };
+             x = MathMethods.Subtract(intArray2);
+             Console.WriteLine(x);
+             var intArray3 = new int[] {2,2,2,2};
+             x = MathMethods.Multiply(intArray3);
+             Console.WriteLine(x);
+
+            
+
+        
+        
         }
     }
 }
